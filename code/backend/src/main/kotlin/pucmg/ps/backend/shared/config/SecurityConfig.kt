724a00f847d05e908ce3b62cff97a9fffa769720
jjoaom <joaomarcos.aquino@hotmail.com>
2026-06-09 15:33:48 -0300

    Fix dependencies for backend

--- code/backend/src/main/kotlin/pucmg/ps/backend/shared/config/SecurityConfig.kt
+++ code/backend/src/main/kotlin/pucmg/ps/backend/shared/config/SecurityConfig.kt
@@ -1,9 +1,11 @@
 package pucmg.ps.backend.shared.config
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider
+import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -12,7 +14,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository
 import org.springframework.security.web.header.writers.StaticHeadersWriter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import pucmg.ps.backend.features.auth.CustomUserDetailsService
 import pucmg.ps.backend.features.auth.jwt.JwtAuthFilter
 
