da5f5b81a959f3d4a95aa7d03bd1fa491b3cbef7
jjoaom <joaomarcos.aquino@hotmail.com>
2026-06-16 23:08:16 -0300

    Fix: Implantation Diagram and added security testing reports

---
+++ code/scripts/sec/run-trivy-mysql.bat
@@ -0,0 +1,20 @@
+@echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+SET SCRIPT_DIR=%~dp0
+SET OUTPUT_DIR=%SCRIPT_DIR%..\..\..\docs\security\trivy
+
+for /f %%i in ('powershell -NoProfile -Command "Get-Date -Format yyyyMMdd-HHmmss"') do set TIMESTAMP=%%i
+
+IF NOT EXIST "%OUTPUT_DIR%" mkdir "%OUTPUT_DIR%"
+
+docker run --rm ^
+  -v /var/run/docker.sock:/var/run/docker.sock ^
+  -v "%OUTPUT_DIR%:/output" ^
+  aquasec/trivy image ^
+  --format template ^
+  --template "@contrib/html.tpl" ^
+  -o "/output/report-mysql-%TIMESTAMP%.html" ^
+  scripts-mysql
+
+pause
